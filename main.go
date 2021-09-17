b76fd1c0815c107b1195ef504ddff549a52fc9be
mehdi <mehdiadq@gmail.com>
2021-09-16 21:57:07 -0700

    init concurrency added

--- main.go
+++ main.go
@@ -6,16 +6,18 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"sync"
 	"time"
 )
 
-func getDrives() []string {
+func getDrives(wg *sync.WaitGroup) []string {
 	d := make([]string, 0)
 
 	for _, drive := range "ABCDEFGHIJKLMNOPQRSTUVWXYZ" {
 		f, err := os.Open(string(drive) + ":\\")
 		if err == nil {
 			d = append(d, string(drive)+":/")
+			wg.Add(1)
 			f.Close()
 		}
 	}
@@ -27,7 +29,9 @@ func main() {
 	start := time.Now()
 	fmt.Println("Started...")
 
-	drives := getDrives()
+	var wg sync.WaitGroup
+
+	drives := getDrives(&wg)
 
 	f, err := os.Create("list.txt")
 	if err != nil {
@@ -36,24 +40,29 @@ func main() {
 	defer f.Close()
 
 	for _, d := range drives {
-		err := filepath.Walk(d, func(path string, info fs.FileInfo, err error) error {
-			if !info.IsDir() {
-				if ext := filepath.Ext(path); ext == ".pdf" {
-					_, err = f.WriteString(path + "\n")
-					if err != nil {
-						log.Fatal(err)
+		go func(drive string) {
+			err := filepath.Walk(drive, func(path string, info fs.FileInfo, err error) error {
+				if !info.IsDir() {
+					if ext := filepath.Ext(path); ext == ".pdf" {
+						_, err = f.WriteString(path + "\n")
+						if err != nil {
+							log.Fatal(err)
+						}
 					}
 				}
+
+				return nil
+			})
+
+			if err != nil {
+				log.Println(err)
 			}
 
-			return nil
-		})
-
-		if err != nil {
-			log.Println(err)
-		}
+			wg.Done()
+		}(d)
 	}
 
+	wg.Wait()
 	fmt.Println("Finished.")
 	duration := time.Since(start)
 	fmt.Println("Elapsed Time:", duration)
